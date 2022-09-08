dcede36a09f90b999f62ed1d1d7eceda781759b0
DaniilCoolUkraine <cd0games2000@gmail.com>
2022-09-08 19:19:00 +0300

    added aplying the noise to tilemap

--- My project/Assets/Scripts/GameController.cs
+++ My project/Assets/Scripts/GameController.cs
@@ -1,18 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class GameController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private NoiseGenerator _noiseGenerator;
+
+    //tilemap to apply noise
+    [SerializeField] private Tilemap levelMap;
+    
+    //needed size of generate map
+    [SerializeField] private int height;
+    [SerializeField] private int width;
+    
+    //density of walls
+    [SerializeField] private int density;
+    
+    //awake to get noise generator
+    private void Awake()
     {
-        
+        _noiseGenerator = gameObject.GetComponent<NoiseGenerator>();
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        DrawNoise();
+    }
+
     void Update()
     {
         
     }
+
+    //function to draw noise tiles on map
+    void DrawNoise()
+    {
+        //temp array to store generated noise
+        TileBase[,] tempTilebaseArray = _noiseGenerator.MakeNoiseMap(density, height, width);
+        
+        //again loop to iterate through 2-dimension array
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                //copy tiles from noise to map and shift it to the center
+                levelMap.SetTile(new Vector3Int(i - height / 2, j - width / 2, 0), tempTilebaseArray[i, j]);
+            }
+        }
+    }
+    
 }
--- My project/Assets/Scripts/NoiseGenerator.cs
+++ My project/Assets/Scripts/NoiseGenerator.cs
@@ -3,15 +3,21 @@ using UnityEngine.Tilemaps;
 
 public class NoiseGenerator : MonoBehaviour
 {
+    //here we assign two tileBase
+    //it will be wall and floor tiles
     [SerializeField] private TileBase floorTile;
     [SerializeField] private TileBase wallTile;
     
+    //create a function, it takes density of noise and size of level map
     public TileBase[,] MakeNoiseMap(int density, int height, int width)
     {
+        //temporary array to store tilebase
         TileBase[,] noiseGrid = new TileBase[height,width];
-
+        
+        //random value to decide type of next tile 
         int random = Random.Range(1, 100);
 
+        //loop to go through 2-dimension array
         for (int i = 0; i < height; i++)
         {
             for (int j = 0; j < width; j++)
@@ -20,6 +26,8 @@ public class NoiseGenerator : MonoBehaviour
                     noiseGrid[i, j] = floorTile;
                 else
                     noiseGrid[i, j] = wallTile;
+                
+                random = Random.Range(1, 100);
             }
         }
         return noiseGrid;
